927a7c451d739d48503e7c7256ee29d71c9d651a
Willian Antunes <wiliam-m-@hotmail.com>
2021-07-01 18:10:19 -0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,4 @@ int main()
      detector->BuildInterpreter("/home/dev-ia/coral/pycoral/test_data/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite", 1);
 
      std::cout<<detector->GetVersion()<<endl;
-
-     std::cout<<"Sucess"<<std::endl;
 }
